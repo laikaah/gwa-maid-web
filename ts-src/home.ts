e881c7bba7866aa817e9e5f64dafd922cf997579
francisdbillones <francisd.billones@gmail.com>
2021-03-14 21:04:08 +0800

    added validation when creating new subject

--- ts-src/home.ts
+++ ts-src/home.ts
@@ -39,32 +39,61 @@ let subjects = cache.getSubjects();
 document.addEventListener('DOMContentLoaded', () => {
     const table: HTMLTableElement = document.querySelector('table');
 
-    const reload: HTMLButtonElement = <HTMLButtonElement> document.getElementById('reload')
+    const reload: HTMLButtonElement = <HTMLButtonElement>document.getElementById('reload')
 
     const form: HTMLFormElement = document.querySelector('form')
 
-    if (subjects != null){
+    if (subjects != null) {
         helpers.writeSubjectsToTable(table, subjects);
     }
 
     reload.onclick = () => {
         api.getSubjects(token).
-        then(subjects => {
-            cache.saveSubjects(subjects);
-            helpers.writeSubjectsToTable(table, subjects);
-        });
+            then(subjects => {
+                cache.saveSubjects(subjects);
+                helpers.writeSubjectsToTable(table, subjects);
+            });
     }
 
     form.onsubmit = (e) => {
         e.preventDefault();
 
-        let subject_name = (<HTMLInputElement>form.elements.namedItem('subject_name')).value;   
+        // check validity for each input field
+        document.querySelectorAll('input').forEach((field) => {
+            if (!field.checkValidity()) {
+                field.reportValidity();
+            }
+        });
 
-        let subject_weight = (<HTMLInputElement>form.elements.namedItem('subject_weight')).value;
+        let subject_name_field = <HTMLInputElement>form.elements.namedItem('subject_name');
+        let subject_name = subject_name_field.value;
+
+        // check that the subject name doesn't already exist
+        if (subjects.map(subject => subject.name).includes(subject_name)) {
+            subject_name_field.setCustomValidity('That name is taken.')
+        } else {
+            subject_name_field.setCustomValidity('');
+        }
+
+        
+        let subject_weight_field = <HTMLInputElement>form.elements.namedItem('subject_weight');
+        let subject_weight = parseInt(subject_weight_field.value);
+
+        let sum_of_subject_weights = subjects.map(subject => subject.weight).reduce((sum, weight) => sum + weight, 0)
+
+        // check that the total weight won't go over 100%
+        if ((sum_of_subject_weights + subject_weight) / 100 > 1) {
+            let total_weight = (sum_of_subject_weights + subject_weight) / 100
+            let total_weight_in_percent = total_weight.toPrecision(2) + '%'
+
+            subject_weight_field.setCustomValidity('Adding this weight would result in a cumulative weight of ' + total_weight_in_percent);
+        } else {
+            subject_weight_field.setCustomValidity('');
+        }
 
         let subject: Subject = {
             name: subject_name,
-            weight: parseInt(subject_weight),
+            weight: subject_weight,
             last_updated: new Date().toLocaleString('en-US'),
             predicted_grade: DEFAULT_GRADE
         };
@@ -73,9 +102,9 @@ document.addEventListener('DOMContentLoaded', () => {
         cache.addSubject(subject);
 
         // if online, save changes to server
-        if (window.navigator.onLine){
+        if (window.navigator.onLine) {
             api.addSubject(token, subject).then((response) => {
-                if (response){
+                if (response) {
                     window.location.reload();
                 } else {
                     alert('Something went wrong.')
